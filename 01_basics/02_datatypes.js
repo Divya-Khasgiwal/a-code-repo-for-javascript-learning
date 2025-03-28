e602cbb3f3bdd2948f6e485eb00dd49820913471
Divya-Khasgiwal <dkhasgiwal@gmail.com>
2025-03-28 10:04:44 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)//we are using node.js not browser
+
+console.log(3+3)
+
+console.log("hitesh")
+
+let name = "div"
+let age = 18
+let isLoggedIn = false
+
+//number 2 to pwer 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value (khali jaanmuch ke)
+//undefined => abhi value assign nahi huyi hai
+//symbol => unique
+
+
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);// object
